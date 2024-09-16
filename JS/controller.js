3b20453127ce8d08cbecc7cd1e94162fd8a71173
YanissCedriik <167817702+YanissCedriik@users.noreply.github.com>
2024-09-17 01:08:24 +0200

    Going to bed

--- JS/controller.js
+++ JS/controller.js
@@ -1,10 +1,9 @@
 //Functions
 
-
-function drawTable(){
-    let HTML = "";
-    for (let i = 0; i < taskList.length; i++) {
-        HTML += /*HTML*/`
+function drawTable() {
+  let HTML = "";
+  for (let i = 0; i < taskList.length; i++) {
+    HTML += /*HTML*/ `
         <tbody>
         <tr>
             <td>${taskList[i].title}</td>
@@ -14,9 +13,10 @@ function drawTable(){
             <td>${taskList[i].status}</td>
         </tr>
         </tbody>
-        `}
-    
-    HTML+= /*HTML*/ `
+        `;
+  }
+
+  HTML += /*HTML*/ `
     <tbody>
     <tr>
             <td><input type="text" placeholder="Example Title..." onchange="tempTask.title = this.value"></td>
@@ -26,81 +26,73 @@ function drawTable(){
             <td><div onclick="addTask()"><img src="Assets/plus-circle-fill.svg"></div></td>
         </tr>
         </tbody>
-        `; 
-        
-        return HTML;
+        `;
+
+  return HTML;
 }
 
-function addTask(){
-    // if løkke som sjekker at tempTask har alle felt fylt ut - hvis ikke, gå ut av funksjonen uten å legge til (return)
-    if(tempTask.title == "" || tempTask.description == "" || tempTask.task == "")return;
+function addTask() {
+  // if løkke som sjekker at tempTask har alle felt fylt ut - hvis ikke, gå ut av funksjonen uten å legge til (return)
+  if (tempTask.title == "" || tempTask.description == "" || tempTask.task == "")
+    return;
 
-    taskList.push(tempTask);
-    updateView();
+  taskList.push(tempTask);
+  updateView();
 
-    tempTask= {
-        title: '',
-        task: '',
-        description: '',
-        deadline: '',
-        status: 'Pending',
-    }
-
+  tempTask = {
+    title: "",
+    task: "",
+    description: "",
+    deadline: "",
+    status: "Pending",
+  };
 }
 
 
 
-
-
-
-function drawLists(){
-    let listHTML = "";
-    for (let i = 0; i < projectList.length; i++) {
-        
-        listHTML += /*HTML*/ `
+function drawLists() {
+  let listHTML = "";
+  for (let i = 0; i < projectList.length; i++) {
+    listHTML += /*HTML*/ `
         <div>${projectList[i]}</div>
-        ` 
-    } 
+        `;
+  }
 
-    listHTML += /*HTML*/`
+  listHTML += /*HTML*/ `
     <div><input type="text" placeholder="New Project..." 
-    onkeypress="tempProject = this.value ; clickPress(event) "></div>
-    `
-    
-    return listHTML;
+    onkeypress="tempProject = this.value ; clickPress(event)"></div>
+    `;
+
+  return listHTML;
 }
 
-function addProjectToList(){
-    
-    if (!tempProject == ""){
-        projectList.push(tempProject)
-        updateView()
-        
-    }
-    
-    tempProject = "";
-};
 
 
+function addProjectToList() {
+  if (!tempProject == "") {
+    projectList.push(tempProject);
+    updateView();
+  }
 
+  tempProject = "";
+}
 
 
 
 
 
 // Carl var "G" å lagde denne
-function clickPress(event) {   
-    if (event.key == "Enter") {
-        addProjectToList();
-    };
+function clickPress(event) {
+  if (event.key == "Enter") {
+    addProjectToList();
+  }
 }
 
 //Han lagde også denne, hva gjør denne egt???
-function CarlSinFunksjon(){
-    let div = document.createElement('div')
-    div.id = "no"
-    div.innerHTML = "\u0043\u0061\u0072\u006C\u0020\u0077\u0061\u0073\u0020\u0068\u0065\u0072\u0065";
-    document.body.appendChild(div)
-
-
+function CarlSinFunksjon() {
+  let div = document.createElement("div");
+  div.id = "no";
+  div.innerHTML =
+    "\u0043\u0061\u0072\u006C\u0020\u0077\u0061\u0073\u0020\u0068\u0065\u0072\u0065";
+  document.body.appendChild(div);
 }
